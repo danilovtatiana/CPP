4f851a19012ad786464867adf1b1f87cbeed5b58
Ceaica Tatiana <ceaica.tatiana@gmail.com>
2021-03-29 17:59:39 +0300

    Syntax,output,comments

---
+++ myfirstproject.cpp
@@ -0,0 +1,13 @@
+/* The code below will print the words Hello World!
+to the screen */
+
+#include <cstdio>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    printf("Hello,World!\n");
+    cout <<"Another way to say hello"<<endl; //endl is the same as /n
+    return 0;
+}
