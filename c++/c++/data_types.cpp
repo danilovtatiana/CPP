86c209bd7871a5e4f982652da5ff3e80b3390d66
Ceaica Tatiana <ceaica.tatiana@gmail.com>
2021-03-31 22:28:27 +0300

    Data Types

---
+++ c++/c++/data_types.cpp
@@ -0,0 +1,50 @@
+//
+//  data_types.cpp
+//  c++
+//
+//  Created by Taniusa :* on 31.03.2021.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //numbers
+    int myNum = 10; //integers
+    float myN = 5.75; //float
+    double myD = 19.99; //double
+    
+    //boolean
+    
+    bool isCodingFun = true;
+    bool isFishTasty = false;
+    
+    //character data types
+    
+    char myGrade = 'B';
+    
+    //ASCII values
+    char a =65, b = 66, c = 67;
+    
+    //strings
+    
+    string greeting = "Hello";
+    
+    cout << myNum <<"\n";
+    cout << myN <<"\n";
+    cout << myD <<"\n";
+    
+    cout << isCodingFun <<"\n";
+    cout << isFishTasty <<"\n";
+    
+    cout <<myGrade <<"\n";
+    
+    cout << a <<"\n";
+    cout << b <<"\n";
+    cout << c <<"\n";
+    
+    cout << greeting <<"\n";
+    
+    return 0;
+}
--- c++/c++/main.cpp
+++ c++/c++/main.cpp
@@ -1,45 +1,46 @@
-/* The code below will print the words Hello World!
-to the screen */
-
-#include <cstdio>
-#include <iostream>
-using namespace std;
-
-int main()
-{
-    // printf("Hello,World!\n");
-    // cout <<"Another way to say hello"<<endl; //endl is the same as /n
-
-    // // variables
-
-    // int x = 6;  //integers
-    // double y = 3.14; //float number 8 bytes (15 digits)
-    // float m = 7.19; // float number 4 bytes (7 digist)
-    // char z = 'z'; //char
-    // string d = "abcd"; //string
-
-    // cout << x <<endl;
-    // cout << y <<endl;
-    // cout << z <<endl;
-    // cout << d <<endl;
-
-    // //text and variables should be separated by insertion operator: <<
-
-    // int myAge = 16;
-    // cout << "I am " <<myAge<< " years old \n";
-
-    //user input
-
-    int a,b;
-    int sum;
-    
-    cout << "Type a number: ";
-    cin >> a;
-    cout << "Type another number: ";
-    cin >> b;
-    sum = a + b;
-
-    cout << "Sum is: " << sum << "\n";
-
-    return 0;
-}
+///* The code below will print the words Hello World!
+//to the screen */
+//
+//#include <cstdio>
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//    // printf("Hello,World!\n");
+//    // cout <<"Another way to say hello"<<endl; //endl is the same as /n
+//
+//    // // variables
+//
+//    // int x = 6;  //integers
+//    // double y = 3.14; //float number 8 bytes (15 digits)
+//    // float m = 7.19; // float number 4 bytes (7 digist)
+//    // char z = 'z'; //char
+//    // string d = "abcd"; //string
+//
+//    // cout << x <<endl;
+//    // cout << y <<endl;
+//    // cout << z <<endl;
+//    // cout << d <<endl;
+//
+//    // //text and variables should be separated by insertion operator: <<
+//
+//    // int myAge = 16;
+//    // cout << "I am " <<myAge<< " years old \n";
+//
+//    //user input
+//
+//    int a,b;
+//    int sum;
+//    
+//    cout << "Type a number: ";
+//    cin >> a;
+//    cout << "Type another number: ";
+//    cin >> b;
+//    sum = a + b;
+//
+//    cout << "Sum is: " << sum << "\n";
+//
+//    return 0;
+//}
+//
