03ff9712f8abe8735f426c80498289ae86de7f58
Ceaica Tatiana <ceaica.tatiana@gmail.com>
2021-04-02 10:20:44 +0300

    if-else statements; short hand if-else

---
+++ c++/c++/if_else_statements.cpp
@@ -0,0 +1,30 @@
+//
+//  if_else_statements.cpp
+//  c++
+//
+//  Created by Taniusa :* on 02.04.2021.
+//
+
+#include "iostream"
+using namespace std;
+
+int main()
+{
+    int x = 20;
+    int y = 30;
+    
+    if (x > y){
+        cout << "x is greater than y" <<"\n";
+    } else if (x < y){
+        cout << "x is smaller than y" <<"\n";
+    } else {
+        cout << "x is equal to y" <<"\n";
+    }
+    
+    //short hand if-else
+    string result = (x > y) ? "x is greater than y" : "x is smaller than y";
+    cout << result <<"\n";
+}
+
+
+
---
+++ c++/c++/if_else_statements.hpp
@@ -0,0 +1,13 @@
+//
+//  if_else_statements.hpp
+//  c++
+//
+//  Created by Taniusa :* on 02.04.2021.
+//
+
+#ifndef if_else_statements_hpp
+#define if_else_statements_hpp
+
+#include <stdio.h>
+
+#endif /* if_else_statements_hpp */
