e204772afc8ed2908686094f46bd9efa6ef8f576
Ceaica Tatiana <ceaica.tatiana@gmail.com>
2021-04-02 11:31:34 +0300

    while/ do-while loops

---
+++ c++/c++/while_loop.cpp
@@ -0,0 +1,28 @@
+////
+////  while_loop.cpp
+////  c++
+////
+////  Created by Taniusa :* on 02.04.2021.
+////
+//
+//#include "iostream"
+//using namespace std;
+//
+//int main ()
+//{
+//    int i = 0;
+//    while (i < 5) {
+//        cout << i << "\n";
+//        i++;
+//    }
+//    
+//    //do-while loop
+//    
+//    int x = 5;
+//    do {
+//        cout << i << "\n";
+//        i++;
+//    }
+//    while (i < 10);
+//}
+//
---
+++ c++/c++/while_loop.hpp
@@ -0,0 +1,13 @@
+//
+//  while_loop.hpp
+//  c++
+//
+//  Created by Taniusa :* on 02.04.2021.
+//
+
+#ifndef while_loop_hpp
+#define while_loop_hpp
+
+#include <stdio.h>
+
+#endif /* while_loop_hpp */
