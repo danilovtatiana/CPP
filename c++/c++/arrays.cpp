68387e2767214648067590e463e0d3956db1d5b4
Ceaica Tatiana <ceaica.tatiana@gmail.com>
2021-04-02 12:22:55 +0300

    Arrays, declare an array, change the elements, for loop in an array

---
+++ c++/c++/arrays.cpp
@@ -0,0 +1,30 @@
+////
+////  arrays.cpp
+////  c++
+////
+////  Created by Taniusa :* on 02.04.2021.
+////
+//
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//    //declare an array
+//    
+//    string cars[3] = {"Volvo", "BMW", "Ford"};
+//    
+//    //access variables from an array
+//    cout << cars[0] << "\n";
+//    
+//    //change an array element
+//    
+//    cars[1] = "Opel";
+//    cout << cars[1] << "\n";
+//    
+//    //arrays and loops
+//    for (int i = 0; i < 3; i++){
+//        //cout << cars[i] << "\n"; //print array's elements
+//        cout << i << ":" << cars[i] << "\n"; // print array's element and its index position
+//    }
+//}
--- c++/c++/break_continue_statements.cpp
+++ c++/c++/break_continue_statements.cpp
@@ -1,31 +1,31 @@
+////
+////  break_continue_statements.cpp
+////  c++
+////
+////  Created by Taniusa :* on 02.04.2021.
+////
 //
-//  break_continue_statements.cpp
-//  c++
+//#include <iostream>
+//using namespace std;
 //
-//  Created by Taniusa :* on 02.04.2021.
+//int main ()
+//{
+//    int i = 0;
+//    while( i < 10){
+//        cout << i << "\n";
+//        i++;
+//        if (i == 4){
+//            break;;
+//        }
+//    }
+//    
+//    int x = 0;
+//    while ( x < 10) {
+//        if (x == 4) {
+//            x++;
+//            continue;
+//        }
+//    } cout << x << "\n";
+//    x++;
+//}
 //
-
-#include <iostream>
-using namespace std;
-
-int main ()
-{
-    int i = 0;
-    while( i < 10){
-        cout << i << "\n";
-        i++;
-        if (i == 4){
-            break;;
-        }
-    }
-    
-    int x = 0;
-    while ( x < 10) {
-        if (x == 4) {
-            x++;
-            continue;
-        }
-    } cout << x << "\n";
-    x++;
-}
-
