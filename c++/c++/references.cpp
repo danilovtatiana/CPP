4bf8c19ea235a8732810bf9bd1af2c411b3be8c9
Ceaica Tatiana <ceaica.tatiana@gmail.com>
2021-04-02 12:33:03 +0300

    References and memory address

---
+++ c++/c++/references.cpp
@@ -0,0 +1,19 @@
+////
+////  references.cpp
+////  c++
+////
+////  Created by Taniusa :* on 02.04.2021.
+////
+//
+//#include <iostream>
+//using namespace std;
+//
+//int main ()
+//{
+//    string food = "Pizza"; //food variable
+//    string &meal = food; // reference to food
+//    
+//    cout << food << "\n"; // output Pizza
+//    cout << meal << "\n"; // output Pizza
+//    cout << &food << "\n"; // output memory address
+//}
