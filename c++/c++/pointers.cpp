5c5844a75bd9169cb258cc92564c330b5a55ba15
Ceaica Tatiana <ceaica.tatiana@gmail.com>
2021-04-02 12:48:12 +0300

    Pointers, reference, dereference

---
+++ c++/c++/pointers.cpp
@@ -0,0 +1,23 @@
+////
+////  pointers.cpp
+////  c++
+////
+////  Created by Taniusa :* on 02.04.2021.
+////
+//
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//    string food = "Pizza"; // a food variable of type string
+//    string* ptr = &food; // a pointer variable, with the name ptr, that stores the addresses of food
+//    
+//    cout << food << "\n"; // output the value of food Pizza
+//    
+//    cout << &food << "\n"; // output the memory address
+//    //reference
+//    cout <<ptr << "\n"; // output the memory address of food with the pointer
+//    //dereference
+//    cout << *ptr << "\n"; //output the value of food with the pointer (Pizza)
+//}
