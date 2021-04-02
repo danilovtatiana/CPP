c83666e301b812081a34d0b11b3ab908b7e8bf01
Ceaica Tatiana <ceaica.tatiana@gmail.com>
2021-04-02 11:52:16 +0300

    foor loop

---
+++ c++/c++/for_loop.cpp
@@ -0,0 +1,21 @@
+//
+//  for_loop.cpp
+//  c++
+//
+//  Created by Taniusa :* on 02.04.2021.
+//
+
+#include "iostream"
+using namespace std;
+int main ()
+
+{
+    for (int i = 0; i < 5; i++) {
+        cout << i << "\n";
+    }
+    
+    for (int i = 0; i <= 10; i += 2) {
+        cout << i << "\n";
+    }
+}
+
