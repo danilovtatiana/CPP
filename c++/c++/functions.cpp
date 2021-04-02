954a6adaee8e05d3e1d7f08e37847e13d6e2b0ed
Ceaica Tatiana <ceaica.tatiana@gmail.com>
2021-04-02 13:02:29 +0300

    functions, parameters

---
+++ c++/c++/functions.cpp
@@ -0,0 +1,26 @@
+////
+////  functions.cpp
+////  c++
+////
+////  Created by Taniusa :* on 02.04.2021.
+////
+//
+//#include <iostream>
+//using namespace std;
+//
+//void myFunction();
+//void myName(string name){ //parameters 
+//    cout <<name << "\n";
+//}
+//
+//int main()
+//{
+//    myFunction(); //call the function
+//    myName("Tatiana");
+//    return 0;
+//}
+////function definition
+//
+//void myFunction(){
+//    cout << "I have just got executed! \n ";
+//}
